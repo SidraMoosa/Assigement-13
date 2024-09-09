704667e0d7dc316068f6830be04c3a2be10b0f96
Sidra Moosa | Front-End Developer <sidramoosa94@gmail.com>
2024-09-09 01:24:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,177 @@
+// Chapter 1 (Alerts)
+// 1.
+ // alert("FirstName: Sidra");
+// alert("LastName: Moosa");
+// alert("Email: sidramoosa94@gmail.com");
+// alert("Phone Number: 123-45678-90");
+// alert("Password: 12345");
+
+// 2.
+//  alert("You're learning Javascript");
+
+// 3.
+// alert("wellcome to my Website");
+
+
+// Chapter 2 (Variables for string)
+// 1.
+ //var firstName = 20;
+// console.log(firstName);
+
+// 2.
+// var num;
+// num = 12345;
+// console.log(num);
+
+// 3.
+// var teamname = "Pakistan";
+// alert(teamname);
+
+// 4.
+// var bestMan = "Charlie";
+// bestMan = "Jhon"
+// console.log(bestMan);
+
+
+// Chapter 3 (Variables for numbers)
+// 1.
+// var caseQty = 10;
+// console.log(caseQty);
+
+// 2.
+// caseQty = 144; 
+// console.log(caseQty);
+
+// 1,2.
+// var caseQty =10;
+// caseQty =144;
+// console.log(caseQty);
+
+// 3.
+// var num = 9;
+// console.log(num);
+
+// 4.
+// var nunber;
+// number = 10+15;
+// console.log(number);
+
+// 5.
+// var merchTotal = 100;
+// var shippingCharge =10;
+// console.log(merchTotal+shippingCharge);
+
+// 6.
+// var number = 5;
+// number = 10+10;
+// console.log(number);
+
+
+
+                        //    PDF Chapters
+
+                        // CHAPTER:01  ALERTS
+
+// 1.                        
+// alert("Wellcome to my website");
+
+// 2.
+// alert("Error! please enter a valid password");
+
+// 3.
+// alert("Wellcome to Js Land... \nHappy Coding");
+
+// 4.
+// alert("Wellcome to JS Land");
+// alert("Happy Coding \n Prevent this page from creating additional dialogs.");
+
+// 5.
+// console.log("Hello.. I can run JS through my web brower's console");
+
+// 7.
+// see html file
+
+
+                        // CHAPTER: 02    VARIABLES FOR STRINGS
+
+// 1. 
+// var username ;
+// console.log(username) ;                      
+
+// 2.
+// var myname = "SidraMoosa";
+// console.log(myname);
+
+// 3.
+// var message;
+// message ="Hello World";
+// alert(message);
+
+// 4. 
+// alert("jhone Deo");
+// alert("15 years old");
+// alert("Certified Mobile Application Development");
+
+// 5
+// alert("PIZZA\n PIZZ\n PIZ\n PI\n P");
+
+// 6.
+// var email = "example@example.com";
+// alert("My email Address is " +email);
+
+// 7.
+// var book ="A smarter way to learn JavaScript."
+// alert("I am typing to learn from the book "+book);
+
+// // 8.
+// document.write("Yah! I can write HTML content through Javascript");
+
+// 9.
+// var string ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(string);
+
+// var string ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// document.write(string);
+
+
+                    // CHAPTER 03: VARIABLES FOR NUMBERS
+
+// 1. 
+// var age = 20;                   
+// alert("I am " + age +" years old");
+
+// 2.
+// var variable ="You have visited this site 14 times";
+// alert(variable);
+
+// 3.
+// var birthyear ="2004";
+// document.write("My birth year is " +birthyear +" <br> Data type of my decleared variable is number");
+
+// 4.
+// var Visitorname ="John Deo";
+// var producttitle ="On XYZ Clothing Store";
+// var quantity =" order 5 T-Shirt(s)";
+// document.write(Visitorname+quantity+producttitle);
+
+
+                //CHAPTER 04: VARIABLE NAMES: LEGAL & ILLEGAL
+
+// 1.
+// var firstname = "Sidra", lastname = "Moosa", city =" Karachi";
+// console.log(firstname +lastname +city);                
+
+// 2.
+//  5 Legal Variable Names
+// 1. userName 
+// 2. first_name
+// 3. totalAmount
+// 4. maxValue
+// 5. minValue
+
+//  5 inLegal Variable Names
+// 1. 1stRank
+// 2. total-amount
+// 3. helo
+// 4. user account
+// 5. #value
